b80fa14375df91a5f359a487013e64e1cfbe3b39
Raghav Sai Cheedalla <raghavsai.cheedalla@dominionenterprises.com>
2018-09-24 15:09:28 -0400

    - Added Network Utility class and Flickr Url Constructions

--- app/src/main/java/com/example/android/favoritephotos/MapsActivity.java
+++ app/src/main/java/com/example/android/favoritephotos/MapsActivity.java
@@ -1,9 +1,8 @@
 package com.example.android.favoritephotos;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -39,8 +38,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                mMap.addMarker(new MarkerOptions().position(latLng));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/android/favoritephotos/utils/JsonUtils.java
@@ -0,0 +1,5 @@
+package com.example.android.favoritephotos.utils;
+
+public final class JsonUtils {
+
+}
